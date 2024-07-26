0655c5273ddaf6426280a7dd1d3fb10233f57daf
Jerome Dalisay <80521805+jeromedalisay@users.noreply.github.com>
2024-07-26 14:31:05 +0800

    Csi UI prod merchant 3 (#50)
    * Add new page and modify Dashboard
    
    New page for Walk In, Employee, Volume Shopper, Bank Promos, GCash
    Dashboard change UI
    
    * Update Router.tsx
    
    * Modify Submit Button
    
    Change the API of submit button to the newly create SubmitAnalyticsWOProoflist. Without condition of isTransfer
    
    * modify datepicker
    
    modify datepicker for analytic, match and portal
    
    * Development for 5 Merchant analytics
    
    Development of WalkIn, Employee, GCash, Volume Shopper, and Bank Promos. Adjusted some code for the UI and data retrieval for multiple merchant code
    
    * Development for 5 Merchant analytics
    
    Development of WalkIn, Employee, GCash, Volume Shopper, and Bank Promos. Adjusted some code for the UI and data retrieval for multiple merchant code
    
    * Fix analytics, match, portal
    
    Fix analytics, match, portal selection date and match adding of employee, walkin, gcash, volume shopper, bank promos
    
    * modification of retrieval merchants transaction
    
    modification of retrieval of amount and details of merchants transaction
    
    * modify bank promo
    
    get all customer code in database
    
    * Add UB and WalkIn Invoice report with generation
    
    * fix weekly report merchant generation
    
    * Enhancement for UB and WalkIn
    
    Reports and Viewing
    
    * Enhancement submit and reload
    
    ---------
    
    Co-authored-by: Richard Baluyut <133842568+chaaard@users.noreply.github.com>

--- src/Pages/_Treasury/UBRenewal/UBRenewal.tsx
+++ src/Pages/_Treasury/UBRenewal/UBRenewal.tsx
@@ -611,6 +611,7 @@ const handleSaveCustomer = async (id: number, remarks: string) => {
         memCode: customerCode,
         userId: Id,
         storeId: [club], 
+        remarks: "ubrenewal",
       }
 
       const refreshAnalytics: AxiosRequestConfig = {
@@ -681,6 +682,7 @@ const handleSaveCustomer = async (id: number, remarks: string) => {
         memCode: customerCode,
         userId: Id,
         storeId: [club], 
+        remarks: "ubrenewal",
       }
 
       const submitAnalytics: AxiosRequestConfig = {
@@ -731,6 +733,7 @@ const handleSaveCustomer = async (id: number, remarks: string) => {
               memCode: customerCode,
               userId: Id,
               storeId: [club], 
+              remarks: "ubrenewal",
             }
         
             const submitgenerate: AxiosRequestConfig = {
